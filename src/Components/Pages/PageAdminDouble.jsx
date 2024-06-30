b3338baad05a698a81b3a4f2c575fe397983d17f
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-07-01 05:17:30 +0700

    fix: update data

--- src/Components/Pages/PageAdminDouble.jsx
+++ src/Components/Pages/PageAdminDouble.jsx
@@ -9,6 +9,15 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  TextField,
+  Button,
+  Container,
+  Grid,
+
 } from '@mui/material';
 import { adminService } from '../../api/services/admin.services';
 import { tableSlice  } from '../../Store/reducer/StatesTable';
@@ -62,33 +71,41 @@ const PageAdminDouble = () => {
   const data = useSelector(state => state.StatesTable.dataOfTable);
   const columns = useSelector(state => state.StatesTable.columnOfTable);
   console.log('получили из', columns)
-
+  let idd;
   // Обработчик событий клика на элементы меню
   const handleClick = async (event, id) => {
     let nameTable;
     switch (id) {
       case '1':
+        idd = 1;
         nameTable = 'getUsers';
         break;
       case '2':
+        idd = 2;
         nameTable = 'getOrders';
         break;
       case '3':
+        idd = 3;
         nameTable = 'getRoles';
         break;
       case '4':
+        idd = 4;
         nameTable = 'getTransactions';
         break;
       case '5':
+        idd = 5;
         nameTable = 'getAddresses';
         break;
       case '6':
+        idd = 6
         nameTable = 'getReviews';
         break;
       case '7':
+        idd = 7
         nameTable = 'getProducts';
         break;
       case '8':
+        idd = 8;
         nameTable = 'getCategories';
         break;
       default:
@@ -107,6 +124,30 @@ const PageAdminDouble = () => {
     prepareRow,
   //} = useTable({ columns, !data ? data :  [{ id: '4', login: 'admin1', password: 'adawd', email: 'efwfewf', role: 'wfefw' }]});
   } = useTable({columns, data})
+
+  const [selectedId, setSelectedId] = useState('');
+  const [selectedColumn, setSelectedColumn] = useState('');
+  const [newValue, setNewValue] = useState('');
+
+  const handleUpdate = () => {
+    if (!selectedId || !selectedColumn) {
+      alert('Пожалуйста, выберите ID и столбец');
+      return;
+    }
+
+    //const data = { select: selectedId, id: idd};
+
+    
+
+    const updatedData = data.map(row => {
+      if (row.id === selectedId) {
+        return { ...row, [selectedColumn]: newValue };
+      }
+      return row;
+    });
+    dispatch(tablesToState1(updatedData));
+  };
+
   return (
     <>
       <AppBar position="static">
@@ -140,33 +181,71 @@ const PageAdminDouble = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Table {...getTableProps()}>
-        <TableHead>
-          {headerGroups.map(headerGroup => (
-            <TableRow {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
-              {headerGroup.headers.map(column => (
-                <TableCell {...column.getHeaderProps()} key={column.id}>
-                  {column.render('Header')}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableHead>
-        <TableBody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <TableRow {...row.getRowProps()} key={row.id}>
-                {row.cells.map(cell => (
-                  <TableCell {...cell.getCellProps()} key={cell.column.id}>
-                    {cell.render('Cell')}
+      <Container>
+      <Grid container spacing={3} alignItems="center" style={{ marginTop: 20 }}>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel>Выберите ID</InputLabel>
+              <Select value={selectedId} onChange={(e) => setSelectedId(e.target.value)}>
+                {data.map(row => (
+                  <MenuItem key={row.id} value={row.id}>{row.id}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel>Выберите столбец</InputLabel>
+              <Select value={selectedColumn} onChange={(e) => setSelectedColumn(e.target.value)}>
+                {columns.map(column => (
+                  <MenuItem key={column.accessor} value={column.accessor}>{column.Header}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              fullWidth
+              label="Новое значение"
+              value={newValue}
+              onChange={(e) => setNewValue(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <Button fullWidth variant="contained" color="primary" onClick={handleUpdate}>
+              Изменить
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Table {...getTableProps()}>
+          <TableHead>
+            {headerGroups.map(headerGroup => (
+              <TableRow {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                {headerGroup.headers.map(column => (
+                  <TableCell {...column.getHeaderProps()} key={column.id}>
+                    {column.render('Header')}
                   </TableCell>
                 ))}
               </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHead>
+          <TableBody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row);
+              return (
+                <TableRow {...row.getRowProps()} key={row.id}>
+                  {row.cells.map(cell => (
+                    <TableCell {...cell.getCellProps()} key={cell.column.id}>
+                      {cell.render('Cell')}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Container>
     </>
   );
 };
--- src/api/services/admin.services.js
+++ src/api/services/admin.services.js
@@ -14,10 +14,10 @@ export const adminService = {
     }
 },
   
-  async postRegistrationUser(data){
+  async postNewData(data){
     try {
       //console.log("Получили на API: ", { data })
-      const res = await instance.post('user/registration', { data } )
+      const res = await instance.post('/adminka/postData', { data } )
       //console.log("От севера на API: ", res.data)
 
       if(!res) {
@@ -28,6 +28,24 @@ export const adminService = {
     catch(error) {
       console.log("API сдох", error)
     }
+  },
+
+  async postDeleteData(data){
+    try {
+		const g = data.id;
+		console.log("Получили на API: ", { g })
+		const data = data.select;
+		const res = await instance.post('/adminka/deteleData', { data } )
+		//console.log("От севера на API: ", res.data)
+
+		if(!res) {
+			console.log('Error 228')
+		}
+		return res.data;
+		} 
+		catch(error) {
+		console.log("API сдох", error)
+		}
   },
 
   async registration(data) {
